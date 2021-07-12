6a42b242f5c9b39a1eb3663111bd41b21b374ead
ashot08 <albertguzeev@yandex.ru>
2021-07-12 19:47:15 +0700

    add roll two dice & rolls statistic

--- index.js
+++ index.js
@@ -15,5 +15,27 @@ function getRandomIndexesForArray(N, M) {
 //console.log( getRandomIndexesForArray(100, 10).sort((a,b)=>a-b) );
 
 /*
-roll two dice
+roll two dice & rolls statistic
+input: rolls count (number)
+out: result statistic(object)
 */
+function rollTwoDice(times){
+    let result = {
+        '0': 0,
+        '1': 0,
+        '2': 0,
+        '3': 0,
+        '4': 0,
+        '5': 0,
+    };
+    for (let i = 0; i < times; i++){
+        let twoRollsResult  = [ getRandomIndexesForArray(6,6)[0],(getRandomIndexesForArray(6,6))[0] ];
+        for(let i of twoRollsResult){
+            result[i]++;
+        }
+
+    }
+    return result;
+}
+console.log(rollTwoDice(1113110));
+
