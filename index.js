58c82de0cbeb21bdf9fdccd91a70c8b64fbfb858
ashot08 <albertguzeev@yandex.ru>
2021-08-03 10:00:20 +0700

    end "Triangle array with multiply" with console.log

--- index.js
+++ index.js
@@ -664,10 +664,16 @@ class TriangleArrayWithMultiply extends TriangleArrayWithAdd{
         for(let i = 0; i < this.length; i++){
             for(let j = 0; j <= i; j++){
                 const index = i*(i+1)/2 + j;
-                for(let k = 0; k <=j; k++){
-                    newArr[index] += this.getCell(k,i)*triangleArray.getCell(k, j);
-                    console.log(newArr[index])
+
+                for(let k = 0; k <= 4; k++){
+                    if(this.getCell(k,i) !== undefined && triangleArray.getCell(j, k) !== undefined){
+                        newArr[index] += this.getCell(k, i) * triangleArray.getCell(j, k);
+                        console.log('a1 - ' + this.getCell(k, i) +' - index ' + index + `(${k}, ${i})`);
+                        console.log('a2 - ' + triangleArray.getCell(j, k) +' - index ' + index + `(${j}, ${k})`);
+                    }
+
                 }
+                console.log('----------------')
             }
         }
         return newArr;
@@ -675,5 +681,5 @@ class TriangleArrayWithMultiply extends TriangleArrayWithAdd{
 }
 const trArr1 = new TriangleArrayWithMultiply(5);
 const trArr2 = new TriangleArrayWithMultiply(5);
-console.log(trArr1);
+
 console.log(trArr1.multiply(trArr2));
